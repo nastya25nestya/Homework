c662389454c449e52726b60a97438626746ea12a
nastya25nestya <knastya2509@mail.ru>
2021-11-24 18:24:30 +0300

    func_max mod to min, func description modified

--- func_max.py
+++ func_max.py
@@ -1,9 +1,9 @@
-def large(arr):
-max_elem = arr[0]
+def minimum(arr):
+min_elem = arr[0]
 for elem in arr:
-if elem > max_elem:
-max_elem = elem
-return max_elem
+if elem < min_elem:
+min_elem = elem
+return min_elem
 
 
 a=[]
@@ -11,5 +11,5 @@ n=int(input()) # ввод количества элементов массива
 print('Введите элементы массива: ')
 for i in range(n):
 a.append(int(input())) # ввод элементов массива
-result = large(a)
-print('Максимальный элемент: ', result)
+result = minimum(a)
+print('Минимальный элемент: ', result)
